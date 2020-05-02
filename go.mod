4303c33029cc33baa87e54203198f0d718d34365
weixiao619 <l243224118@163.com>
2020-05-02 12:17:49 +0800

    update

--- go.mod
+++ go.mod
@@ -4,12 +4,24 @@ go 1.13
 
 require (
 	github.com/Knetic/govaluate v3.0.0+incompatible // indirect
+	github.com/Shopify/sarama v1.23.1 // indirect
 	github.com/fsouza/go-dockerclient v1.6.4 // indirect
-	github.com/go-kratos/kratos v0.4.2 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.0.1-0.20190118093823-f849b5445de4 // indirect
 	github.com/hashicorp/go-version v1.2.0 // indirect
 	github.com/hyperledger/fabric v1.4.4
 	github.com/hyperledger/fabric-amcl v0.0.0-20200128223036-d1aa2665426a // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/miekg/pkcs11 v1.0.3 // indirect
 	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7 // indirect
+	github.com/pierrec/lz4 v1.0.2-0.20190131084431-473cd7ce01a1 // indirect
+	github.com/sirupsen/logrus v1.4.2 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.3.2 // indirect
 	github.com/sykesm/zap-logfmt v0.0.3 // indirect
+	golang.org/x/net v0.0.0-20191011234655-491137f69257 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/sys v0.0.0-20191105231009-c1f44814a5cd // indirect
+	golang.org/x/tools v0.0.0-20191105231337-689d0f08e67a // indirect
+	google.golang.org/genproto v0.0.0-20191216205247-b31c10ee225f // indirect
+	gopkg.in/jcmturner/goidentity.v3 v3.0.0 // indirect
 )
--- trace.go
+++ trace.go
@@ -1,23 +1,66 @@
-package trace
+package main
 
 import (
 	"encoding/json"
 	"fmt"
-	"foodTrace/model"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
-type Food struct {
+type FoodChainCode struct {
 }
 
-func (a *Food) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("Food Init")
-	return shim.Success([]byte("Init success!"))
+//food数据结构体
+type FoodInfo struct {
+	FoodID      string    `json:FoodID`      //食品ID
+	FoodProInfo ProInfo   `json:FoodProInfo` //生产信息
+	FoodIngInfo []IngInfo `json:FoodIngInfo` //配料信息
+	FoodLogInfo LogInfo   `json:FoodLogInfo` //物流信息
 }
 
-func (a *Food) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+type FoodAllInfo struct {
+	FoodID      string    `json:FoodId`
+	FoodProInfo ProInfo   `json:FoodProInfo`
+	FoodIngInfo []IngInfo `json:FoodIngInfo`
+	FoodLogInfo []LogInfo `json:FoodLogInfo`
+}
+
+//生产信息
+type ProInfo struct {
+	FoodName     string `json:FoodName`     //食品名称
+	FoodSpec     string `json:FoodSpec`     //食品规格
+	FoodMFGDate  string `json:FoodMFGDate`  //食品出产日期
+	FoodEXPDate  string `json:FoodEXPDate`  //食品保质期
+	FoodLOT      string `json:FoodLOT`      //食品批次号
+	FoodQSID     string `json:FoodQSID`     //食品生产许可证编号
+	FoodMFRSName string `json:FoodMFRSName` //食品生产商名称
+	FoodProPrice string `json:FoodProPrice` //食品生产价格
+	FoodProPlace string `json:FoodProPlace` //食品生产所在地
+}
+type IngInfo struct {
+	IngID   string `json:IngID`   //配料ID
+	IngName string `json:IngName` //配料名称
+}
+
+type LogInfo struct {
+	LogDepartureTm string `json:LogDepartureTm` //出发时间
+	LogArrivalTm   string `json:LogArrivalTm`   //到达时间
+	LogMission     string `json:LogMission`     //处理业务(储存or运输)
+	LogDeparturePl string `json:LogDeparturePl` //出发地
+	LogDest        string `json:LogDest`        //目的地
+	LogToSeller    string `json:LogToSeller`    //销售商
+	LogStorageTm   string `json:LogStorageTm`   //存储时间
+	LogMOT         string `json:LogMOT`         //运送方式
+	LogCopName     string `json:LogCopName`     //物流公司名称
+	LogCost        string `json:LogCost`        //费用
+}
+
+func (a *FoodChainCode) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	return shim.Success(nil)
+}
+
+func (a *FoodChainCode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	fn, args := stub.GetFunctionAndParameters()
 
 	switch fn {
@@ -42,9 +85,9 @@ func (a *Food) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	return shim.Error(fmt.Sprintf("unsupported function: %s", fn))
 }
 
-func (a *Food) addProInfo(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (a *FoodChainCode) addProInfo(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	var err error
-	var FoodInfos model.FoodInfo
+	var FoodInfos FoodInfo
 
 	if len(args) != 10 {
 		return shim.Error("Incorrect number of arguments.")
@@ -76,10 +119,10 @@ func (a *Food) addProInfo(stub shim.ChaincodeStubInterface, args []string) pb.Re
 	return shim.Success(nil)
 }
 
-func (a *Food) addIngInfo(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (a *FoodChainCode) addIngInfo(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
-	var FoodInfos model.FoodInfo
-	var IngInfoitem model.IngInfo
+	var FoodInfos FoodInfo
+	var IngInfoitem IngInfo
 
 	if (len(args)-1)%2 != 0 || len(args) == 1 {
 		return shim.Error("Incorrect number of arguments")
@@ -108,10 +151,10 @@ func (a *Food) addIngInfo(stub shim.ChaincodeStubInterface, args []string) pb.Re
 
 }
 
-func (a *Food) addLogInfo(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (a *FoodChainCode) addLogInfo(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
 	var err error
-	var FoodInfos model.FoodInfo
+	var FoodInfos FoodInfo
 
 	if len(args) != 11 {
 		return shim.Error("Incorrect number of arguments.")
@@ -142,7 +185,7 @@ func (a *Food) addLogInfo(stub shim.ChaincodeStubInterface, args []string) pb.Re
 	return shim.Success(nil)
 }
 
-func (a *Food) getFoodInfo(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (a *FoodChainCode) getFoodInfo(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	if len(args) != 1 {
 		return shim.Error("Incorrect number of arguments.")
 	}
@@ -153,10 +196,10 @@ func (a *Food) getFoodInfo(stub shim.ChaincodeStubInterface, args []string) pb.R
 	}
 	defer resultsIterator.Close()
 
-	var foodAllinfo model.FoodAllInfo
+	var foodAllinfo FoodAllInfo
 
 	for resultsIterator.HasNext() {
-		var FoodInfos model.FoodInfo
+		var FoodInfos FoodInfo
 		response, err := resultsIterator.Next()
 		if err != nil {
 			return shim.Error(err.Error())
@@ -180,7 +223,7 @@ func (a *Food) getFoodInfo(stub shim.ChaincodeStubInterface, args []string) pb.R
 	return shim.Success(jsonsAsBytes)
 }
 
-func (a *Food) getProInfo(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (a *FoodChainCode) getProInfo(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
 	if len(args) != 1 {
 		return shim.Error("Incorrect number of arguments.")
@@ -192,10 +235,10 @@ func (a *Food) getProInfo(stub shim.ChaincodeStubInterface, args []string) pb.Re
 	}
 	defer resultsIterator.Close()
 
-	var foodProInfo model.ProInfo
+	var foodProInfo ProInfo
 
 	for resultsIterator.HasNext() {
-		var FoodInfos model.FoodInfo
+		var FoodInfos FoodInfo
 		response, err := resultsIterator.Next()
 		if err != nil {
 			return shim.Error(err.Error())
@@ -213,7 +256,7 @@ func (a *Food) getProInfo(stub shim.ChaincodeStubInterface, args []string) pb.Re
 	return shim.Success(jsonsAsBytes)
 }
 
-func (a *Food) getIngInfo(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (a *FoodChainCode) getIngInfo(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
 	if len(args) != 1 {
 		return shim.Error("Incorrect number of arguments.")
@@ -226,9 +269,9 @@ func (a *Food) getIngInfo(stub shim.ChaincodeStubInterface, args []string) pb.Re
 	}
 	defer resultsIterator.Close()
 
-	var foodIngInfo []model.IngInfo
+	var foodIngInfo []IngInfo
 	for resultsIterator.HasNext() {
-		var FoodInfos model.FoodInfo
+		var FoodInfos FoodInfo
 		response, err := resultsIterator.Next()
 		if err != nil {
 			return shim.Error(err.Error())
@@ -246,9 +289,9 @@ func (a *Food) getIngInfo(stub shim.ChaincodeStubInterface, args []string) pb.Re
 	return shim.Success(jsonsAsBytes)
 }
 
-func (a *Food) getLogInfo(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (a *FoodChainCode) getLogInfo(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
-	var LogInfos []model.LogInfo
+	var LogInfos []LogInfo
 
 	if len(args) != 1 {
 		return shim.Error("Incorrect number of arguments.")
@@ -262,7 +305,7 @@ func (a *Food) getLogInfo(stub shim.ChaincodeStubInterface, args []string) pb.Re
 	defer resultsIterator.Close()
 
 	for resultsIterator.HasNext() {
-		var FoodInfos model.FoodInfo
+		var FoodInfos FoodInfo
 		response, err := resultsIterator.Next()
 		if err != nil {
 			return shim.Error(err.Error())
@@ -279,8 +322,8 @@ func (a *Food) getLogInfo(stub shim.ChaincodeStubInterface, args []string) pb.Re
 	return shim.Success(jsonsAsBytes)
 }
 
-func (a *Food) getLogInfo_l(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var Loginfo model.LogInfo
+func (a *FoodChainCode) getLogInfo_l(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	var Loginfo LogInfo
 
 	if len(args) != 1 {
 		return shim.Error("Incorrect number of arguments.")
@@ -294,7 +337,7 @@ func (a *Food) getLogInfo_l(stub shim.ChaincodeStubInterface, args []string) pb.
 	defer resultsIterator.Close()
 
 	for resultsIterator.HasNext() {
-		var FoodInfos model.FoodInfo
+		var FoodInfos FoodInfo
 		response, err := resultsIterator.Next()
 		if err != nil {
 			return shim.Error(err.Error())
@@ -313,7 +356,7 @@ func (a *Food) getLogInfo_l(stub shim.ChaincodeStubInterface, args []string) pb.
 }
 
 func main() {
-	err := shim.Start(new(Food))
+	err := shim.Start(new(FoodChainCode))
 	if err != nil {
 		fmt.Printf("Error starting Food chaincode: %s ", err)
 	}
